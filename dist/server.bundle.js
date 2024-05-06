a4a5f6aa48f7f684a5e875b70547809b0ea7694f
Scarlet1107 <shougo.ushiro@gmail.com>
2024-05-06 15:33:34 +0900

    first commit

---
+++ dist/server.bundle.js
@@ -0,0 +1,89 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./server/index.js":
+/*!*************************!*\
+  !*** ./server/index.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("var express = __webpack_require__(/*! express */ \"express\");\nvar React = __webpack_require__(/*! react */ \"react\");\nvar ReactDOMServer = __webpack_require__(/*! react-dom/server */ \"react-dom/server\");\nvar App = __webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '../../TodoList_withTypeScript/src/App'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\nvar app = express();\napp.get('/', function (req, res) {\n  var appString = ReactDOMServer.renderToString(React.createElement(App));\n  res.send(\"<!DOCTYPE html>\\n<html>\\n<head>\\n  <title>My React App</title>\\n</head>\\n<body>\\n  <div id=\\\"root\\\">\".concat(appString, \"</div>\\n</body>\\n</html>\"));\n});\nvar PORT = process.env.PORT || 9000;\napp.listen(PORT, function () {\n  console.log(\"Server is running on http://localhost:\".concat(PORT));\n});\n\n//# sourceURL=webpack://my-express-app/./server/index.js?");
+
+/***/ }),
+
+/***/ "express":
+/*!**************************!*\
+  !*** external "express" ***!
+  \**************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("express");
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "react-dom/server":
+/*!***********************************!*\
+  !*** external "react-dom/server" ***!
+  \***********************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react-dom/server");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./server/index.js");
+/******/ 	
+/******/ })()
+;
---
+++ server/index.js
@@ -0,0 +1,13 @@
+// Expressモジュールをインポート
+const express = require('express');
+const app = express();
+
+// ルートURLにアクセスされたときに実行される処理
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// 9000ポートでサーバを起動
+app.listen(9000, () => {
+  console.log('Application is running on http://localhost:9000');
+});
